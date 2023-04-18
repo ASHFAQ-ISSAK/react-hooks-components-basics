790ecb6b8605a650284b616197b43c7de3f6af0f
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-04-18 10:51:22 +0300

    Created 3 components,Article,Comment,Disclaimer and a Completemessage component to render the components

--- src/Article.js
+++ src/Article.js
@@ -2,8 +2,26 @@ import React from "react";
 
 function Article() {
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration</div>
   );
 }
 
-export default Article;
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+}
+
+function Disclaimer() {
+  return <div>It's true. I was the recource</div>;
+}
+
+function Completemessage() {
+  return (
+    <div>
+      <Article />
+      <Comment />
+      <Disclaimer />
+    </div>
+  );
+}
+
+export default Completemessage;
